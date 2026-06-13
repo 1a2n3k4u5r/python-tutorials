5431941137090f5cbc32bc39cddd8df9ae534776
1a2n3k4u5r <ankur1236876@gmail.com>
2026-06-13 06:06:51 +0530

    Add examples of class and constructor usage in Python with detailed explanations

---
+++ OOPs in Python/Class and Object.py
@@ -0,0 +1,19 @@
+# Class is a blueprint for creating objects.
+# class Name is always start with capital letter.
+
+# creating class
+class Student:
+    name = "ankur yadav"
+
+# creating object(instance)
+s1 = Student()
+print(s1.name)
+
+# Example
+class Car:
+    color = "blue"
+    brand = "merecedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
---
+++ OOPs in Python/constructor or init.py
@@ -0,0 +1,47 @@
+# Constructor
+# All classes have a function called __init__(), which is always executed when the object is being initiated.
+# This function is invovked(executed) at the time of object creation.
+# Constructor always take an argument/parameter which is called self parameter.
+
+# Example
+class Student:
+
+    def __init__(self, fullname):
+        self.name = fullname
+        print("adding new student in Database..")
+   
+
+s1 = Student("ankur") # here paranthesis is used to call the constructor
+print(s1.name) #output = ankur
+ # Self is the first parameter in the constructor which is the argument
+
+# NOTE = The (self) parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+# the data that is stored inside the class or inside the object  is calle attributes.
+
+# Example
+class Student:
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("adding new student in Database..")
+   
+s1 = Student("ankur", 97)
+print(s1.name, s1.marks) 
+
+s2 = Student("arjun", 32)
+print(s2.name, s2.marks) 
+
+# Constructor are two type:
+# 1) default constructor : which has only one parameter.
+# Ex:
+def __init__(self):
+    pass
+
+
+# 2) parameterized constructor : which has many or more than one parameter.
+# Ex:
+def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("adding new student in Database..")
