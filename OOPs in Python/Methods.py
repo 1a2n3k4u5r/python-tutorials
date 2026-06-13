8ad9ace02e32ae8cdeffe86cac4823d79f835daa
1a2n3k4u5r <ankur1236876@gmail.com>
2026-06-13 19:44:15 +0530

    Add static method example and enhance Account class with debit and credit methods

--- OOPs in Python/Methods.py
+++ OOPs in Python/Methods.py
@@ -52,3 +52,102 @@ s1.get_avg()
 
 s1.name = "ironman"
 s1.get_avg()
+
+
+# **** Static Method ****
+# Methods that don't use the self parameter(work at class level)
+
+# class student:
+@staticmethod   #decorator which is used to remove the error in this code.
+
+def college():
+   print("ABC College")
+
+# Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function, without permanentely modifying it.
+
+# There are four pillars in the OOPs concept.
+
+# 1) Abstraction - Hiding the implemention details of a class and only shoeing the essential features to the user.
+class Car :
+   def __init__(self):
+      self.acc = False
+      self.brk = False
+      self.clutch = False
+
+   def start(self):
+         self.clutch = True
+         self.acc = True
+         print("car started..")
+
+car1 = Car()
+car1.start() #car started..
+
+   
+
+# 2) Encapsulation - Wrapping data and functions into a single unit(object).
+
+# 3) Inheritance.
+# 4) Polymorphism.
+
+# Practice question
+# Create Account class with 2 attributes-balance and account no.
+# Create methods for debit,credit & printing the balance.
+
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("Debited:", amount)
+        print("Current balance:", self.balance)
+
+    def credit(self, amount):
+        self.balance += amount
+        print("Credited:", amount)
+        print("Current balance:", self.balance)
+
+acc1 = Account(5000)
+
+acc1.debit(1000)
+acc1.credit(500)
+# **** del keyword **** #
+# used to delete object properties or object itself.
+ 
+# del s1.name #delete attribute 
+# del s1  # delete object
+
+class Student:
+   def __init__(self, name):
+      self.name = name
+
+s1 = Student("shradha")
+print(s1.name)
+del(s1.name)
+
+
+# **** Private(like) Attributes and Methods **** 
+#conceptual implementation in python- Private attributes & methods are meant to be used only within the class and are not accessible from outside the class.
+ 
+# class Student:
+#    def __init__(self,name):
+#       self.name = name
+
+# s1 = Student("shradha")
+# print(s1.name)
+
+
+class Account:
+   def __init__(self, acc_no, acc_pass):
+    self.acc_no = acc_no
+    self.acc_pass = acc_pass
+
+   def reset_pass(self):
+      print(self.__acc_pass)
+
+acc1 = Account("12345", "abcde")
+
+print(acc1.acc_no)
+print(acc1.acc_pass)
+
+# To make any attribute private we use a two Underscore(__).
