e99ade6be5e6e295cc750f441ce4afadad22012a
1a2n3k4u5r <ankur1236876@gmail.com>
2026-06-16 14:33:09 +0530

    Add comprehensive documentation for pip, including installation, package management, and usage examples

---
+++ Module packages and libaries/Pip.py
@@ -0,0 +1,32 @@
+# pip stands for "Pip installs Packages or Preferred Installer Program". It is package manager for python that allows you to install,update, and manage Python libraries(packages) from the Python Package Index(PypI).
+
+# Think of pip as an app store for python libraries.You use it to search,install,and manage Python tools,just like downloading apps on your phone.
+
+# when you use pip install<package_name>,it:
+#Connects to PyPI(Python Package_name) online.
+# Download the specified library or package.
+#Install it into your Python enivornment.
+
+# To install packages, we use: pip install <library_name>
+
+#Example: install pandas to work on dataframe:
+# pip install pandas
+
+# pip = Shop → It downloads and installs Python packages.
+# import = Use → It brings an installed package into your program.
+
+
+# List Packages
+# pip list
+# Use the list command to list all the packages installed on your system.
+
+
+# Remove a Package
+# pip uninstall <library_name>
+# Use the uninstall command to remove a package.
+
+
+# Check if PIP is Installed
+# pip --version
+
+
