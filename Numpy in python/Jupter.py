70b0ff125a5bd2f561edaeb593c13a8990adef2f
1a2n3k4u5r <ankur1236876@gmail.com>
2026-06-27 05:41:31 +0530

    Add initial Numpy tutorial files including Jupyter notebook and Python scripts with basic content

---
+++ Numpy in python/Jupter.py
@@ -0,0 +1,8 @@
+# JUPYTER NOTEBOOK
+# The Jupyter notebook is an open-source web application that allows you to create and share documents that contain live code,equations,visualizations and narrative text.
+
+# The Notebook has support for over 40 programming language, including Python, R, julia, and Scala.
+
+# Notebooks can be shared with others using email, Dropbox, Github,and the Jupyter Notebook Viewer.
+
+# Your code can produce rich,interactive output: HTML,images,videos,LaTex,and custom MIME types.
---
+++ Numpy in python/Numpy.py
@@ -0,0 +1,7 @@
+# Why use NUMPY
+# NumPy space bachata hai.
+# NUmpy provides efficient storage.
+# It also provides better ways of handling data for processing.
+# It is fast.
+# It is easy to learn.
+# NumPy uses relatively less memory to store data.
